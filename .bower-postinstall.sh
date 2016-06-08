b59fc433c42b4dfa8ed4dffd2ae9d323a3203b2a
Will Donohoe <wdonohoe@google.com>
2016-06-08 13:28:14 +0100

    Added functionality to add the grad hand when hovering over slides, and grabbing hand when dragging. This only happens if the enableGrabCursor property is truthy

--- .bower-postinstall.sh
+++ .bower-postinstall.sh
@@ -2,4 +2,4 @@
 #
 cp bower_components/Swiper/dist/css/swiper.min.css swiper-styles.html
 cat swiper-styles.html | pbcopy && (echo '<dom-module id="swiper-styles">'; echo '  <template>'; echo '    <style>') > swiper-styles.html && pbpaste >> swiper-styles.html;
-(echo ""; echo "    </style>"; echo "  </template>"; echo "</dom-module>") >> swiper-styles.html;
+(echo ""; echo ":host[enable-grab-cursor] .swiper-slide {cursor:-webkit-grab;cursor:-moz-grab;cursor:grab;}    </style>"; echo "  </template>"; echo "</dom-module>") >> swiper-styles.html;
